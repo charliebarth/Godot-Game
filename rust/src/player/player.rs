a90b8374eb2c63fb3b4d7485d68e4a522287f426
Charlie <charliepbarth@gmail.com>
2024-09-10 13:23:52 -0400

    Added current state and last state fields

--- rust/src/player/player.rs
+++ rust/src/player/player.rs
@@ -8,17 +8,29 @@ use godot::classes::ICharacterBody2D;
 // use godot::meta::FromGodot;
 use godot::prelude::*;
 
+use super::enums::player_states::PlayerStates;
+
 // const MAX_JUMP_HEIGHT: f32 = 300.0;
 
 #[derive(GodotClass)]
 #[class(base=CharacterBody2D)]
 struct Player {
     base: Base<CharacterBody2D>,
+    cur_state: PlayerStates,
+    last_state: PlayerStates,
 }
 
 #[godot_api]
 impl ICharacterBody2D for Player {
     fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base }
+        Self {
+            base,
+            cur_state: PlayerStates::Idle,
+            last_state: PlayerStates::Fall,
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        self.cur_state.trigger(&self.last_state)
     }
 }
