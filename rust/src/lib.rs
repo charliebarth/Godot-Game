62373a2a3080637e1865a283457b4911dcfaf0a1
Charles Barth <charliepbarth@gmail.com>
2024-09-09 21:08:10 -0400

    Added code to stop Godot complaining about an empty lib file

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// use godot::builtin::StringName;
+// use godot::builtin::Vector2;
+// use godot::classes::AnimatedSprite2D;
+use godot::classes::CharacterBody2D;
+use godot::classes::ICharacterBody2D;
+// use godot::classes::Input;
+// use godot::classes::ProjectSettings;
+// use godot::meta::FromGodot;
+use godot::prelude::*;
+
+// const MAX_JUMP_HEIGHT: f32 = 300.0;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=CharacterBody2D)]
+struct Player {
+    base: Base<CharacterBody2D>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[godot_api]
+impl ICharacterBody2D for Player {
+    fn init(base: Base<CharacterBody2D>) -> Self {
+        Self { base }
     }
 }
