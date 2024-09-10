2d3af8cf0e9c5f40dbbc7701b5bdc8e2421339c6
Charles Barth <charliepbarth@gmail.com>
2024-09-09 22:27:01 -0400

    Moved player class into its own file

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,16 @@
-// use godot::builtin::StringName;
-// use godot::builtin::Vector2;
-// use godot::classes::AnimatedSprite2D;
-use godot::classes::CharacterBody2D;
-use godot::classes::ICharacterBody2D;
-// use godot::classes::Input;
-// use godot::classes::ProjectSettings;
-// use godot::meta::FromGodot;
 use godot::prelude::*;
+
+mod player {
+
+    pub mod player;
+    mod traits {
+        pub mod player_state;
+    }
+
+    mod enums {
+        pub mod player_states;
+    }
+}
 
 // const MAX_JUMP_HEIGHT: f32 = 300.0;
 
@@ -14,16 +18,3 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
-
-#[derive(GodotClass)]
-#[class(base=CharacterBody2D)]
-struct Player {
-    base: Base<CharacterBody2D>,
-}
-
-#[godot_api]
-impl ICharacterBody2D for Player {
-    fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base }
-    }
-}
---
+++ rust/src/player/player.rs
@@ -0,0 +1,24 @@
+// use godot::builtin::StringName;
+// use godot::builtin::Vector2;
+// use godot::classes::AnimatedSprite2D;
+use godot::classes::CharacterBody2D;
+use godot::classes::ICharacterBody2D;
+// use godot::classes::Input;
+// use godot::classes::ProjectSettings;
+// use godot::meta::FromGodot;
+use godot::prelude::*;
+
+// const MAX_JUMP_HEIGHT: f32 = 300.0;
+
+#[derive(GodotClass)]
+#[class(base=CharacterBody2D)]
+struct Player {
+    base: Base<CharacterBody2D>,
+}
+
+#[godot_api]
+impl ICharacterBody2D for Player {
+    fn init(base: Base<CharacterBody2D>) -> Self {
+        Self { base }
+    }
+}
