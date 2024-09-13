5ae49b811a4f37b73662f2ca3fca4ca1ebed8cde
Charles Barth <charliepbarth@gmail.com>
2024-09-13 19:42:29 -0400

    Removed turn around and tied move speed to strength of joystick push

--- rust/src/player/player_states/run.rs
+++ rust/src/player/player_states/run.rs
@@ -2,7 +2,7 @@ use godot::{builtin::StringName, classes::Input, obj::WithBaseField};
 
 use crate::player::{player::Player, traits::player_state::PlayerState};
 
-use super::{fall::Fall, idle::Idle, jump::Jump, turn_around::TurnAround};
+use super::{fall::Fall, idle::Idle, jump::Jump};
 
 #[derive(Clone)]
 pub struct Run;
@@ -16,14 +16,13 @@ impl PlayerState for Run {
         }
 
         player.set_dir(horizontal_dir);
-        player.apply_horizontal_velocity(horizontal_dir, 125.0);
+        player.apply_horizontal_velocity(horizontal_dir, 140.0);
     }
 
     fn update(&self, player: &mut Player) {
         let horizontal_dir = player.get_horizontal_movement();
 
         if horizontal_dir == 0.0 {
-            // TODO: Replace with decelerate state
             player.set_state(Box::new(Idle));
         } else if Input::singleton().is_action_just_pressed(StringName::from("jump"))
             && player.base().is_on_floor()
@@ -31,9 +30,7 @@ impl PlayerState for Run {
             player.set_state(Box::new(Jump));
         } else if !player.base().is_on_floor() {
             player.set_state(Box::new(Fall));
-        } else if horizontal_dir != player.get_dir() {
-            player.set_state(Box::new(TurnAround));
-        } else if horizontal_dir == player.get_dir() {
+        } else if horizontal_dir.signum() == player.get_dir().signum() {
             player.set_state(Box::new(Run));
         }
     }
