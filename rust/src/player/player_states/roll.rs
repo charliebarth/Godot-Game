3ac04d630dc90b1df4e45414dc49e04805441c50
Charles Barth <charliepbarth@gmail.com>
2024-09-16 15:43:15 -0400

    Implemented and tested roll

--- rust/src/player/player_states/roll.rs
+++ rust/src/player/player_states/roll.rs
@@ -1,16 +1,16 @@
-use godot::obj::WithBaseField;
-
-use crate::player::{player::Player, player_states::land::Land, traits::player_state::PlayerState};
+use crate::player::{player::Player, traits::player_state::PlayerState};
 
 #[derive(Clone)]
 pub struct Roll;
 
 impl PlayerState for Roll {
-    fn enter(&self, _player: &mut Player) {}
+    fn enter(&self, player: &mut Player) {
+        player.get_sprite().set_speed_scale(1.0);
+    }
 
     fn update(&self, player: &mut Player) {
-        if player.base().is_on_floor() {
-            player.set_state(Box::new(Land));
+        if player.is_anim_finished() {
+            player.set_state(player.get_previous_state());
         }
     }
 
@@ -18,7 +18,7 @@ impl PlayerState for Roll {
         Box::new(Roll)
     }
 
-    fn as_str(&self, player: &mut Player) -> &str {
-        "fall"
+    fn as_str(&self, _player: &mut Player) -> &str {
+        "roll"
     }
 }
