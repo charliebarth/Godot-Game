6732642d41fac3b392f3d4e1932c3cf11baa1430
Charles Barth <charliepbarth@gmail.com>
2024-09-18 20:12:57 -0400

    Testing

--- rust/src/player/input_manager.rs
+++ rust/src/player/input_manager.rs
@@ -12,5 +12,12 @@ impl INode2D for InputManager {
         Self { base }
     }
 
-    fn input(&mut self, event: Gd<InputEvent>) {}
+    fn input(&mut self, event: Gd<InputEvent>) {
+        godot_print!("Input event: {}", event.as_text());
+        godot_print!("Is pressed: {}", event.is_pressed());
+        godot_print!("Is released: {}", event.is_released());
+        // is_release is useful for buttons but not triggers or sticks but those have a value
+        // triggers return true for is pressed when over half pull and false when below half pull
+        // this might be tied to something like deadzone in settings but I'm not sure yet
+    }
 }
