cd7e9df9bd8a9cc97f69d1abe99a3f2ad0dd6c05
trinpanda <149008043+trinpanda@users.noreply.github.com>
2024-09-19 23:19:12 -0400

    Fixed functions, builds now

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,33 +1,12 @@
-// use godot::builtin::StringName;
-// use godot::builtin::Vector2;
-// use godot::classes::AnimatedSprite2D;
-use godot::classes::CharacterBody2D;
-use godot::classes::ICharacterBody2D;
-// use godot::classes::Input;
-// use godot::classes::ProjectSettings;
-// use godot::meta::FromGodot;
 use godot::prelude::*;
-
-// const MAX_JUMP_HEIGHT: f32 = 300.0;
 
 pub mod metal_reserve_bar_manager;
 
 pub mod metal_bar;
 
+
+
 struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
-
-#[derive(GodotClass)]
-#[class(base=CharacterBody2D)]
-struct Player {
-    base: Base<CharacterBody2D>,
-}
-
-#[godot_api]
-impl ICharacterBody2D for Player {
-    fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base }
-    }
-}
