b1406957d0e610cc0648d5b60088b6e952c822a9
Charles Barth <charliepbarth@gmail.com>
2024-09-25 21:27:49 -0400

    Added BurnType which is held by each enum variant

--- rust/src/player/enums/metal_events.rs
+++ rust/src/player/enums/metal_events.rs
@@ -1,14 +1,45 @@
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
+
+pub enum BurnType {
+    Burn,
+    LowBurn,
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub enum MetalEvents {
-    Pewter,
+    Pewter(BurnType),
 }
 
 impl MetalEvents {
-    // Method to convert from string to the corresponding event
+    /// Method to convert from string to the corresponding event
+    ///
+    /// # Arguments
+    ///
+    /// * `button` - The string to convert to a metal event
+    ///
+    /// # Returns
+    ///
+    /// * `Some(MetalEvents)` - The corresponding metal event with the burn BurnType
+    /// * `None` - If the string does not match any metal event
     pub fn from_string(button: &str) -> Option<MetalEvents> {
         match button {
-            "pewter" => Some(MetalEvents::Pewter),
+            "pewter" => Some(MetalEvents::Pewter(BurnType::Burn)),
             _ => None,
+        }
+    }
+
+    /// Returns the burn type of the metal event
+    pub fn get_burn_type(&self) -> BurnType {
+        match self {
+            MetalEvents::Pewter(burn_type) => burn_type.clone(),
+        }
+    }
+
+    /// Returns the low burn variant of the metal event
+    pub fn get_low_burn_variant(&self) -> MetalEvents {
+        match self {
+            MetalEvents::Pewter(BurnType::Burn) => MetalEvents::Pewter(BurnType::LowBurn),
+            MetalEvents::Pewter(BurnType::LowBurn) => MetalEvents::Pewter(BurnType::LowBurn),
         }
     }
 }
