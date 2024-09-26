ec75c9ca3ec8eb32ca8edf7e0cd167ab58fba780
Charles Barth <charliepbarth@gmail.com>
2024-09-25 21:28:06 -0400

    Made functions take mutable reference to self

--- rust/src/player/traits/metal.rs
+++ rust/src/player/traits/metal.rs
@@ -1,7 +1,7 @@
 use crate::player::player::Player;
 
 pub trait Metal {
-    fn update(&self, player: &mut Player);
-    fn burn(&self, player: &mut Player);
-    fn low_burn(&self, player: &mut Player);
+    fn update(&mut self, player: &mut Player);
+    fn burn(&mut self, player: &mut Player);
+    fn low_burn(&mut self, player: &mut Player);
 }
