053f77e5a81eb8409367a1b2b9b77195a591dda9
trinpanda <149008043+trinpanda@users.noreply.github.com>
2024-10-03 14:30:23 -0400

    some work on setting up metal reserve manager to function

--- rust/src/ui/metal_reserve_bar_manager.rs
+++ rust/src/ui/metal_reserve_bar_manager.rs
@@ -7,12 +7,13 @@
 /// Version : 09/22/2024
 
 use godot::prelude::*;
-use godot::classes::{IVBoxContainer, VBoxContainer};  // Import Node and VBoxContainer
+use godot::classes::{IVBoxContainer, VBoxContainer, TextureProgressBar};  // Import Node and VBoxContainer
 pub use crate::ui::metal_bar::MetalBar;
 
 use std::collections::HashMap;
 
 const MAX_BARS_ON_SCREEN: u8 = 4;
+const TOTAL_BARS: u8 = 10; 
 
 #[derive(GodotClass)]
 #[class(base=VBoxContainer)]
@@ -35,12 +36,33 @@ impl IVBoxContainer for  MetalReserveBarManager {
 
     fn ready(&mut self) { 
         // Create Metals that are auto added to VBox based on the keybound metals 
-        
+        for i in 0..TOTAL_BARS {
+            let mut bar = MetalBar::new_alloc();
+            self.base_mut().add_child(bar);
+        }
+        self.setup_metals();
     }
 
 }
 
 impl MetalReserveBarManager{
+
+    fn setup_metals(&mut self) {
+        // Get all the children of the player 
+        let children: Array<Gd<Node>> = self.base.to_gd().get_children(); 
+        for i in 0..children.len() {
+            let child : Gd<Node> = children.get(i).expect("");
+            if let Ok(mut bar) = child.try_cast::<MetalBar>() {
+                    let mut bar_mut = bar.bind_mut();
+                    bar_mut.set_name("Iron");
+                    bar_mut.set_texture(""); 
+                    bar_mut.hide();
+                } else {
+                    godot_print!("Failed to cast node to MetalBar");
+                }
+    
+        }
+    }
 
     pub fn add_metals(&mut self){
         godot_print!("METALS ADDED")
