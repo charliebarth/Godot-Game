846b55ab4ae5b7431db1b5585d50dc125e087c24
Charles Barth <charliepbarth@gmail.com>
2024-11-09 18:07:01 -0500

    Detect nearby metal objects

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -2,3 +2,15 @@ extends Player
 
 func _on_owner_vis_animation_finished() -> void:
 	self.set_anim_finished()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("is_metal"):
+		var metal = body as MetalObject
+		self.add_metal_object(metal)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.has_method("is_metal"):
+		var metal = body as MetalObject
+		self.remove_metal_object(metal)
