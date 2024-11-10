f22ef25e15f32f74c501d909881d2abb4e4b79f6
trinpanda <149008043+trinpanda@users.noreply.github.com>
2024-11-09 20:01:26 -0500

    trying to fix allomantic lines drawing properly

--- rust/src/ui/allomantic_line.rs
+++ rust/src/ui/allomantic_line.rs
@@ -26,7 +26,6 @@ impl ILine2D for AllomanticLine {
 
     /// Constructor for the Allomantic Line
     fn init(base: Base<Line2D>) -> Self {
-
         Self {
             base,
             metal: None,
@@ -36,40 +35,70 @@ impl ILine2D for AllomanticLine {
     }
 
     fn physics_process(&mut self, delta: f64) {
-        // Get the player position
-        let player_pos = self.player.as_mut().unwrap().get_position() + OFFSET;
+        self.draw();
 
-        // Get the metal position 
-        let target_pos = self.metal.as_mut().unwrap().get_position() + OFFSET;
+        // // Get the player position
+        // let player_pos = self.player.as_mut().unwrap().get_global_position() + OFFSET;
 
+        // // Get the metal position 
+        // let target_pos = self.metal.as_mut().unwrap().get_global_position() + OFFSET;
 
-        // Update the line's points
-        // self.base_mut().draw_line(player_pos, target_pos, Color::from_rgb(173.,216.,230.));
-        self.base_mut().set_points((&[player_pos, target_pos]).into());
-        // self.line.as_mut().unwrap().draw_line(player_pos, target_position);
+        // self.base_mut().clear_points();
+        // // self.base_mut().draw_line(player_pos, target_pos, Color::from_rgb(173.,216.,230.))
+
+        // // Update the line's points
+        // // self.base_mut().draw_line(player_pos, target_pos, Color::from_rgb(173.,216.,230.));
+        // self.base_mut().set_points((&[player_pos, target_pos]).into());
         
     }
 
     fn ready(&mut self){
         self.base_mut().set_physics_process(true);
     }
+
 }
 
+#[godot_api]
 impl AllomanticLine {
+
+    #[func]
+    fn draw(&mut self){
+        if let (Some(metal), Some(player)) = (self.metal.as_ref(), self.player.as_ref()) {
+            let start = metal.get_global_position() + OFFSET;
+            let end = player.get_global_position() + OFFSET;
+
+            godot_print!("Drawing line from {:?} to {:?}", start, end);
+
+            // Draw the line between the positions
+            self.base_mut().clear_points();  // Clear previous points
+            self.base_mut().add_point(start);
+            self.base_mut().add_point(end);
+
+            // Optionally set the color and line width
+            self.base_mut().set_default_color(Color::from_rgb(173.0, 216.0, 230.0));
+            self.base_mut().set_width(2.0);  // Set the line width if desired
+        }
+        
+    }
 
     pub fn initialize_fields(&mut self, metal: Gd<Area2D>, player: Gd<CharacterBody2D>){
         self.metal = Some(metal);
         self.player = Some(player);
+        self.physics_process(0.);
     }
 
-    pub fn setup(&mut self){
-        let start = self.metal.as_mut().unwrap().get_position();
-        let end = self.player.as_mut().unwrap().get_position();
+    // pub fn setup(&mut self){
+    //     let start = self.metal.as_mut().unwrap().get_position();
+    //     let end = self.player.as_mut().unwrap().get_position();
 
-        // self.base_mut().draw_line(start, end, Color::from_rgb(173.,216.,230.));
+    //     // self.base_mut().draw_line(start, end, Color::from_rgb(173.,216.,230.));
 
-        // self.base_mut().add_point(start + OFFSET);
-        // self.base_mut().add_point(end + OFFSET);
+    //     // self.base_mut().add_point(start + OFFSET);
+    //     // self.base_mut().add_point(end + OFFSET);
+    // }
+
+    #[func]
+    pub fn get_metal(&self) -> Option<Gd<Area2D>> {
+        self.metal.clone()
     }
-
 }
