bf7c512bda5709bab10446b171c6171197ad4e66
trinpanda <149008043+trinpanda@users.noreply.github.com>
2024-11-11 09:03:14 -0500

    allomantic line blue and lowered to eye level (11//10)

--- rust/src/ui/allomantic_range.rs
+++ rust/src/ui/allomantic_range.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 /// Represents an Allomantic Line.
 /// 
 /// Author : Trinity Pittman
@@ -52,45 +54,40 @@ impl AllomanticRange {
         if body.has_method(StringName::from("is_metal")){
             godot_print!("IS METAL (entering): {}", body_name); 
             let mut line: Gd<AllomanticLine> = AllomanticLine::new_alloc();
-                line.set_visible(true);
-                line.bind_mut().initialize_fields(metal_potential, parent);
-                // line.set_modulate(Color { r: (1.5), g: (1.5), b: (1.5), a: (1.) });
-                line.set_self_modulate(Color { r: (1.5), g: (1.5), b: (1.5), a: (1.) });
-                // line.set_position(Vector2::new(0.0,0.0));
-                // line.bind_mut().draw();
-    
-                // line.bind_mut().setup();
-                self.base_mut().add_child(line);
+            line.set_visible(true);
+            line.bind_mut().initialize_fields(metal_potential, parent);
+            // line.set_modulate(Color { r: (1.5), g: (1.5), b: (1.5), a: (1.) });
+            // line.set_self_modulate(Color { r: (1.5), g: (1.5), b: (1.5), a: (1.) });
+            // line.set_position(Vector2::new(0.0,0.0));
+            // line.bind_mut().draw();
+
+            // line.bind_mut().setup();
+            self.base_mut().add_child(line);
         } else {
             godot_print!("Something other than a metal object entered the allomantic range.");
         }
+
     }       
 
-    // #[func]
-    // fn exit_range(&mut self, body: Gd<Area2D>) {
-    //     let body_name = body.get_name();
-    //     godot_print!("Alomantic range entered by {body_name}");
+    #[func]
+    fn exit_range(&mut self, body: Gd<Area2D>) {
+        let body_name = body.get_name();
+        godot_print!("Alomantic range exited by {body_name}");
 
-    //     // Find this nodes parent for later
-    //     let parent: Gd<CharacterBody2D> = self.base_mut().get_owner().unwrap().cast::<CharacterBody2D>();
-    //     let metal: Gd<Area2D> = body.clone();
+        let metal: Gd<Area2D> = body.clone();
 
-    //     let children: Array<Gd<Node>> = parent.get_children(); 
-    //     for i in 0..children.len() { 
-    //         // Go through the children and find the `AllomanticLine`
-    //         let child : Gd<Node> = children.get(i).expect("");
-    //         if child.get_name().to_string() == "AllomanticLine" {
-
-    //             if let Ok(mut line) = child.try_cast::<AllomanticLine>() {
-
-    //                 if line.get_metal() == metal {
-    //                     line.queue_free();
-    //                 }
-                    
-    //             } else {
-    //                 godot_print!("Failed to cast node to AllomanticLine");
-    //             }
-    //         }
-    //     }
-    // }
+        let children: Array<Gd<Node>> = self.base_mut().get_children(); 
+        for i in 0..children.len() { 
+            // Go through the children and find the line
+            let child : Gd<Node> = children.get(i).expect("");
+            // godot_print!("CHILD NAME {}", child.get_name());
+            if let Ok(mut line) = child.try_cast::<AllomanticLine>(){
+                if line.bind_mut().get_metal().unwrap().get_name() == metal.get_name() {
+                    line.queue_free();
+                    godot_print!("REMOVED METAL {}", metal.get_name())
+                }
+            }
+            
+        }
+    }
 }
