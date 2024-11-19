d8bfdd9f626fbdc1af4a17f966069c0b4b0af4ac
Charles Barth <charliepbarth@gmail.com>
2024-11-18 21:15:19 -0500

    move new hud elements to owners screen only

--- godot/scripts/metal_line.gd
+++ godot/scripts/metal_line.gd
@@ -1,20 +1,9 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _draw() -> void:
+func _ready() -> void:
 	var player: Player = self.get_parent() as Player
-	var points = player.get_line_points()
+	var layer_num = (player.get_player_id() * 2)
 	
-	if points.is_empty():
-		return
-	
-	var player_pos = to_local(self.get_global_position())
-	var line_points = PackedVector2Array()
-	for target_point in points:
-		line_points.append(player_pos)
-		line_points.append(to_local(target_point))
-	
-	var color = Color(0.117647, 0.564706, 1, 0.7)
-	draw_multiline(line_points, color, 2.0)
-	
+	self.visibility_layer = 1 << layer_num
+	self.light_mask = 1 << layer_num | 1
