cd1172773cd8b08af7a30196f9443b0a348848c4
trinpanda <149008043+trinpanda@users.noreply.github.com>
2024-11-18 20:02:37 -0500

    try to start implementing coin as a throwable

--- rust/src/items/coin.rs
+++ rust/src/items/coin.rs
@@ -1,4 +1,4 @@
-use godot::classes::{Area2D, IArea2D};
+use godot::classes::{RigidBody2D, IRigidBody2D};
 /// Represents a coin.
 ///
 /// Author : Trinity Pittman
@@ -9,27 +9,44 @@ use crate::player::player::Player;
 
 const SPEED: f64 = 25.0;
 
+#[derive(PartialEq)]
+pub enum CoinState {
+    Idle, 
+    PickedUp, 
+    Thrown,
+}
+
+
 /// Represents a coin
 #[derive(GodotClass)]
-#[class(base=Area2D)]
+#[class(base=RigidBody2D)]
 pub struct Coin {
-    base: Base<Area2D>,
+    base: Base<RigidBody2D>,
+    state: CoinState,
+    weight: i32,
 }
 
 #[godot_api]
-impl IArea2D for Coin {
+impl IRigidBody2D for Coin {
     /// Constructor for a Coin
-    fn init(base: Base<Area2D>) -> Self {
-        Self { base }
+    fn init(base: Base<RigidBody2D>) -> Self {
+        Self {  
+            base,
+            state: CoinState::Idle,
+            weight: 10,
+        }
+    }
+
+    fn ready(&mut self) {
+        godot_print!("Coin at position {}", self.base_mut().get_global_position());
+
+        self.base_mut().set_contact_monitor(true);
+        self.base_mut().set_max_contacts_reported(1);
     }
 
     // TODO Unfinished
     fn physics_process(&mut self, delta: f64) {
-        let transform: Transform2D = self.base_mut().get_global_transform();
-        let position: Vector2 = self.base_mut().get_position();
-
-        // let new_pos: Vector2 = idk maybe tranform.x * speed
-        // self.base_mut().set_position(position + new_pos);
+        
     }
 }
 
@@ -42,16 +59,44 @@ impl Coin {
     ///      body (Gd<Node2D>): the Node that enters this coin
     #[func]
     fn coin_pickup(&mut self, body: Gd<Node2D>) {
-        let body_name = body.get_name();
-        godot_print!("Coin entered by {body_name}"); // Prints who picked up the coin
+        if self.state == CoinState::Idle {
+            self.set_state(CoinState::PickedUp);
+            let body_name = body.get_name();
+            godot_print!("Coin entered by {body_name}"); // Prints who picked up the coin
 
-        if let Ok(mut player) = body.try_cast::<Player>() {
-            player.bind_mut().adjust_coins(1); // Dereference and call the method
-            self.base_mut().queue_free(); // Remove the coin from the scene
-        } else {
-            godot_print!("Something other than player entered the coin.");
+            if let Ok(mut player) = body.try_cast::<Player>() {
+                player.bind_mut().adjust_coins(1); // Dereference and call the method
+                self.base_mut().queue_free(); // Remove the coin from the scene
+            } else {
+                godot_print!("Something other than player entered the coin.");
+            }
+            let mut coin = Coin::new_alloc();
         }
-        let mut coin = Coin::new_alloc();
+    }
+
+    fn set_state(&mut self, state: CoinState) {
+        self.state = state;
+    }
+
+    #[func]
+    pub fn throw(&mut self, force: Vector2, dir: Vector2) {
+        // If in PickedUp state
+        if self.state == CoinState::PickedUp {
+            self.set_state(CoinState::Thrown);
+            
+
+            // Throw it? add velocity? 
+            self.base_mut().apply_impulse(dir * force);
+
+
+        }
+    }
+
+    pub fn drop(&mut self) {
+        // drop the coin when it hits something 
+        self.set_state(CoinState::Idle);
+
+        // change velocity to zero ? 
     }
 
     #[func]
