0afb0f7dc3f51cee1c1bf20e9fdd1a7f9d9d79dd
Charles Barth <charliepbarth@gmail.com>
2024-11-20 21:59:38 -0500

    added torch sfx

--- rust/src/player_manager.rs
+++ rust/src/player_manager.rs
@@ -186,6 +186,7 @@ impl PlayerManager {
         let mut p1_container = SubViewportContainer::new_alloc();
         let mut p1_viewport = SubViewport::new_alloc();
         p1_viewport.set_use_hdr_2d(true);
+        p1_viewport.set_as_audio_listener_2d(true);
 
         split_screen_one.set_name("SplitScreenOne".into());
         p1_container.set_name("PlayerOneContainer".into());
@@ -222,6 +223,7 @@ impl PlayerManager {
         let mut p2_container = SubViewportContainer::new_alloc();
         let mut p2_viewport = SubViewport::new_alloc();
         p2_viewport.set_use_hdr_2d(true);
+        p2_viewport.set_as_audio_listener_2d(true);
 
         split_screen_two.set_name("SplitScreenTwo".into());
         p2_container.set_name("PlayerTwoContainer".into());
@@ -273,6 +275,7 @@ impl PlayerManager {
         let mut p3_container = SubViewportContainer::new_alloc();
         let mut p3_viewport = SubViewport::new_alloc();
         p3_viewport.set_use_hdr_2d(true);
+        p3_viewport.set_as_audio_listener_2d(true);
 
         p3_container.set_name("PlayerThreeContainer".into());
         p3_viewport.set_name("PlayerThreeViewport".into());
@@ -327,6 +330,7 @@ impl PlayerManager {
         let mut p4_container = SubViewportContainer::new_alloc();
         let mut p4_viewport = SubViewport::new_alloc();
         p4_viewport.set_use_hdr_2d(true);
+        p4_viewport.set_as_audio_listener_2d(true);
 
         p4_container.set_name("PlayerFourContainer".into());
         p4_viewport.set_name("PlayerFourViewport".into());
