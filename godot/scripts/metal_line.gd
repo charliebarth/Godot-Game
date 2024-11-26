5d83b228609e359d3da7868f69898d939db85d7a
Charles Barth <charliepbarth@gmail.com>
2024-11-25 21:22:41 -0500

    removed old scripts

--- godot/scripts/metal_line.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _ready() -> void:
-	var player: Player = self.get_parent() as Player
-	var layer_num = (player.get_player_id() * 2)
-	
-	self.visibility_layer = 1 << layer_num
-	self.light_mask = 1 << layer_num | 1
