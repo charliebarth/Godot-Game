7dc8bc4c5278030804ff1ec548e09b7d1b7e7892
Charles Barth <charliepbarth@gmail.com>
2024-12-10 02:23:01 -0500

    Most of the player states documented. Rust will auto fetch documentation from the PlayerState trait for function implementations that do not have their own docs.

--- rust/src/player/traits/player_state.rs
+++ rust/src/player/traits/player_state.rs
@@ -1,6 +1,19 @@
 use crate::player::player::Player;
 
+/// This trait defines the functions that a player state must implement.
 pub trait PlayerState {
+    /// This function is called when the player enters the state.
+    /// It is responsible for any one time logic that should occur in the state.
+    /// It does not check for any state transitions.
+    ///
+    /// # Arguments
+    /// * `player` - The player owner of the state.
     fn enter(player: &mut Player);
+
+    /// This function is called every frame the player is in the state.
+    /// It is responsible for updating the player's state and checking for state transitions.
+    ///
+    /// # Arguments
+    /// * `player` - The player owner of the state.
     fn update(player: &mut Player);
 }
