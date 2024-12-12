c01523bcfd67f041d2dc6a648d9eba9f00d387c8
Charles Barth <charliepbarth@gmail.com>
2024-12-11 23:58:02 -0500

    PlayerState docs note

--- rust/src/player/player_states/crouch_end.rs
+++ rust/src/player/player_states/crouch_end.rs
@@ -2,6 +2,12 @@ use crate::player::{
     enums::player_states::PlayerStates, player::Player, traits::player_state::PlayerState,
 };
 
+// NOTE: This and most of the other states have not had documentation added
+// because Rust will automatically derive the documentation from the PlayerState trait.
+// The functions in these traits work nearly identically in each state and just have slitghtly different conditions
+// for swapping to a new state.
+//
+
 #[derive(Clone, Copy)]
 pub struct CrouchEnd;
 
