dedced356f43a895cf73f48e3119fcb3439eaf05
Charles Barth <charliepbarth@gmail.com>
2025-01-26 20:06:58 -0500

    improved flicker effect and added slightly randomized animation speed

--- godot/scripts/candle.gd
+++ godot/scripts/candle.gd
@@ -1,40 +1,24 @@
-extends Node2D
+extends AnimatedSprite2D
 
-@onready var point_light: PointLight2D = $PointLight2D
+@onready var point_light: PointLight2D = $"../PointLight2D"
 
-var target_energy: float
-var current_energy: float
-var transition_time: float
-var timer: Timer
+var new_energy: float
+
 
 func _ready() -> void:
-	var animation = get_node("AnimatedSprite2D") as AnimatedSprite2D
-	var delay = randf_range(0.0, 1.2)
-	await get_tree().create_timer(delay).timeout # Wait for the delay
-	var animation_name = "candleFlame" # Set the animation name
-	animation.play(animation_name) # Start playing the animation
-	flicker()
+	if self.is_visible_in_tree():
+		new_energy = randf_range(0.4, 1.0)
+		var delay = randf_range(0.0, 1.2)
+		await get_tree().create_timer(delay).timeout # Wait for the delay
+		play("candleFlame") # Start playing the animation
 
-func flicker():
-	print("flick")
-	target_energy = randf_range(0.35, 0.9) # Pick a new energy level in the range
-	current_energy = point_light.energy
-	transition_time = randf_range(0.1, 0.3) # Time to transition to the new energy level
-	timer = Timer.new()
-	timer.wait_time = 0.01
-	timer.one_shot = false
-	timer.connect("timeout", Callable(self, "_update_light_energy"))
-	add_child(timer)
-	timer.start()
-
-func _update_light_energy():
-	var step = (target_energy - current_energy) / (transition_time / timer.wait_time)
-	current_energy += step
-
-	if abs(target_energy - current_energy) < 0.01:
-		point_light.energy = target_energy
-		timer.queue_free()
-		# Start the next flicker after reaching the target
-		flicker()
-	else:
-		point_light.energy = current_energy
+func _on_frame_changed() -> void:
+	if self.frame != 0 && self.frame % 10 == 0:
+		new_energy = randf_range(0.4, 1.1)
+	elif point_light.energy > new_energy:
+		point_light.energy -= (self.frame % 10) * 0.008
+	elif point_light.energy <= new_energy:
+		point_light.energy += (self.frame % 10) * 0.008
+	
+	if self.frame != 0 && self.frame % 15 == 0:
+		self.speed_scale = randf_range(0.8, 1.5)
