2233ac9e48fe104bd6faf7964aeb8221d51602d7
Charles Barth <charliepbarth@gmail.com>
2025-02-07 00:40:16 -0500

    starting refactor of fog of war

--- godot/scripts/point_light_2d.gd
+++ godot/scripts/point_light_2d.gd
@@ -5,4 +5,4 @@ extends PointLight2D
 func _on_player_id_changed() -> void:
 	var player: Player = self.get_parent() as Player
 	var layer_num = (player.get_player_id() * 2)
-	self.range_item_cull_mask = (1 << layer_num | 1 << (layer_num - 1))
+	self.range_item_cull_mask = 1 << layer_num
