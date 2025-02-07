0226e135e8fe04a006abae05a51be6474bb4f430
trinpanda <149008043+trinpanda@users.noreply.github.com>
2025-02-07 16:25:40 -0500

    ui customization start

---
+++ godot/scenes/ui_placement.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var button_group = ButtonGroup.new()
+
+$..
+
+var positions = []
+
+func _ready():
+	for button in $VBoxContainer/PanelContainer/GridContainer.get_children():
+		if button is Button:
+			button.button_group = button_group
+	
+	button_group.connect("pressed", on_button_press)
+	
+	
+
+func on_button_press(button: BaseButton):
+	print("button pressed %s" %[button.name])
+	
+	
+	
