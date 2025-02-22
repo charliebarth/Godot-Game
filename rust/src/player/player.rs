bc10e3919f37f4ad6ea001a75460f991ceb9597a
Charles Barth <charliepbarth@gmail.com>
2025-02-22 01:58:00 -0500

    Added params to tin_activated

--- rust/src/player/player.rs
+++ rust/src/player/player.rs
@@ -820,7 +820,7 @@ impl Player {
 
     /// A signal that is emitted by the player when it is using tin
     #[signal]
-    pub fn tin_activated();
+    pub fn tin_activated(&self, light_level: f32, transition_time: f64);
 
     /// If passed true, the player turns on its timer to count down before the player is removed from the game
     /// If passed false, the player turns off its timer meaning it is no longer disconnected
