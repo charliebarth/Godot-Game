d6ca0af778ce802e4e4f0f7d2d3463f73cb915f0
trinpanda <149008043+trinpanda@users.noreply.github.com>
2025-02-25 15:26:06 -0500

    key bind settings persist

--- godot/scripts/controls.gd
+++ godot/scripts/controls.gd
@@ -15,24 +15,73 @@ func _ready():
 	
 	button_group.connect("pressed", on_button_press)	
 	
+	set_keybinds_from_config()
+	
 	# Load Keybinds for Player 1 on startup 
 	load_keybinds_for_device(0)
 	
+func set_keybinds_from_config():
+	var keybind_settings = ConfigFileHandler.load_all_keybind_settings()
+	print("SETTINGS  ", keybind_settings)
+	# TODO Set settings based on what was loaded 
+	
+	var actions = ["jump", "sprint", "roll", "attack", "throw", "low_burn", 
+				"pewter", "iron", "steel"]
+	# Erase current bindings
+	for action in actions:
+		InputMap.action_erase_events(action)
+		
+	# Add keybinding for each player 
+	for player in keybind_settings: 
+		for action in player: 
+			InputMap.action_add_event(action, player[action])
+			
 	
 func load_keybinds_for_device(id: int):
-	var keybind_settings = ConfigFileHandler.load_keybind_settings(id)
+	var keybind_settings = load_keybind_settings(id)
 	print(keybind_settings, "\n")
 	
 	for el in v_box_container.get_children():
 		if el.has_method("set_button_text"):
-			var latest_event = get_action_event_keybound(el.action_name, 
-														button_group
-														.get_pressed_button()
-														.name
-														.to_int()-1)
+			var latest_event = get_action_event_keybound(
+				el.action_name, 
+				button_group.get_pressed_button().name.to_int() - 1)
 			print("LATEST EV: ", latest_event)
 			el.set_button_text(keybind_settings, latest_event)
-	
+
+
+## Gets the keybind settings for a player based on id 
+## 
+## id (int) - Represents the id of a player (Player 1 has an id of 0) 
+func load_keybind_settings(id: int):
+	print("loading keybindings for player %s" %[id])
+	var keybind_settings = {}
+	var events = ["jump", "sprint", "roll", "attack", "throw", "low_burn", 
+				"pewter", "iron", "steel"]
+
+	for event in events: 
+		var key_name = null
+		var backup = null
+		
+		for key in InputMap.action_get_events(event):
+			#print("E: %s\tK: %s\tI: %s" %[event, key, key.device])
+			if key.device == id:
+				key_name = key.as_text()
+				#if key is InputEventJoypadButton:
+					#var button_name = InputEventJoypadButton.get_joy_button_string(key.button_index)
+					#print("Generic button name: %s" % button_name)
+
+			elif key.device == -1: # Defaults to all devices
+				backup = key.as_text()
+
+		if key_name != null:
+			keybind_settings[event] = key_name
+		elif backup != null:
+			keybind_settings[event] = backup 
+		else:
+			keybind_settings[event] = "Unbound"
+					
+	return keybind_settings
 
 ## Called when a button from the Player Group is pressed. Sets the keybind 
 ## buttons to the proper name for the player button pressed. 
@@ -54,6 +103,9 @@ func get_action_event_keybound(event: String, id: int) -> InputEvent:
 			#print("NO -> E: %s\tK: %s\tI: %s" %[event, key, key.device])
 	return null
 
+## When the apply button is pressed, save the graphics setting.
+func _on_apply_pressed() -> void:
+	ConfigFileHandler.save_keybind_settings()
 	
 	
 	
