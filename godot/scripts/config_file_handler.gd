0f585baf151e9e6de909a5a81d1e3bed78d280e7
trinpanda <149008043+trinpanda@users.noreply.github.com>
2025-02-27 12:33:57 -0500

    Documentation

--- godot/scripts/config_file_handler.gd
+++ godot/scripts/config_file_handler.gd
@@ -1,14 +1,25 @@
 extends Node
+## Handles the logic for saving and loading to and from the config file. 
+## 
+## @author Charles Barth 
+## @author Trinity Pittman 
+## @version Spring 2025
 
+## A reference to the config file
 var config = ConfigFile.new()
+## The path to the config file 
 const SETTINGS_FILE_PATH = "user://settings.ini"
 
+## Stores the resolution types 
 var resolutions = [Vector2i(1920, 1080), Vector2i(1920,1200), 
 				Vector2i(1080,960), Vector2i(1280, 720), Vector2i(800,600)]
-var events = ["jump", "sprint", "roll", "attack", "throw", "low_burn", 
+## Stores the action types 
+var actions = ["jump", "sprint", "roll", "attack", "throw", "low_burn", 
 				"pewter", "iron", "steel"]
 
-## Called when the node enters the scene tree for the first time.
+
+## Called when the node enters the scene tree for the first time. Sets the 
+## values that should be in the config file if it doesn't exist. 
 func _ready() -> void:
 	if !FileAccess.file_exists(SETTINGS_FILE_PATH):
 		config.set_value("audio", "Master", 0.6)
@@ -25,26 +36,37 @@ func _ready() -> void:
 		
 		config.set_value("ui", "size", 1)
 		config.set_value("ui", "opacity", 1)
-		config.set_value("ui", "pos_x", -475)
-		config.set_value("ui", "pos_y", -270)
+		config.set_value("ui", "pos", 0)
+		
+		#TODO set the values for keybinds 
 	
 		config.save(SETTINGS_FILE_PATH)
 	else:
 		config.load(SETTINGS_FILE_PATH)
 
+
 ## Loads the setting based on the string passed in, for example "audio" or "ui".
-func load_settings_helper(type: String):
+## 
+## @param type The name of the section to load the settings for
+## @returns A dictionary of the settings for the specified section
+func load_settings_helper(type: String) -> Dictionary:
 	var settings = {}
 	for key in config.get_section_keys(type):
 		settings[key] = config.get_value(type, key)
 	return settings
 
+
 ## Save the audio setting to the config file.
+##
+## @param key The name of the audio setting being saved
+## @param value The value it is being saved as
 func save_audio_setting(key: String, value: float) -> void:
 	config.set_value("audio", key, value)
 	config.save(SETTINGS_FILE_PATH)
 
-func save_graphics_setting():
+
+## Saves the graphics settings to the config file. 
+func save_graphics_setting() -> void:
 	config.set_value(
 		"graphics", 
 		"fullscreen", 
@@ -72,10 +94,13 @@ func save_graphics_setting():
 	)
 
 	config.save(SETTINGS_FILE_PATH)
-	print("saved graphics settings")
 
-	
-func save_ui_settings(size: float, opacity: float, pos: int,):
+## Saves the UI settings to the config file. 
+## 
+## @param size The size to save 
+## @param opacity The opacity to save 
+## @param pos The position index to save 
+func save_ui_settings(size: float, opacity: float, pos: int) -> void:
 	config.set_value(
 		"ui",
 		"size",
@@ -91,31 +116,28 @@ func save_ui_settings(size: float, opacity: float, pos: int,):
 		"pos",
 		pos
 	)
-	#config.set_value(
-		#"ui",
-		#"pos_y",
-		#pos_y
-	#)
 	
 	config.save(SETTINGS_FILE_PATH)
-	print("saved ui settings")
 
-## UNUSED 
-## 
-func save_keybind_settings():
+
+## Saves the keybind settings for every player to the config file
+func save_keybind_settings() -> void:
 	for i in 8: # There are 8 players
-		for event in events:	# Go through the list of events 
-			for key in InputMap.action_get_events(event): # Thru bound keys 
+		for action in actions:	# Go through the list of actions 
+			for key in InputMap.action_get_events(action): # Thru bound keys 
 				if key.device == i:		# Find devices bound key
 					config.set_value(
 						str("keybinds", i),
-						event,
+						action,
 						JSON.stringify(serialize_keybind(key))
 					)
 	config.save(SETTINGS_FILE_PATH)
-	print("saved keybind settings")
+
 
 ## Takes in an InputEvent and parses it into a storable dictionary 
+##
+## @param key The input event to serialize 
+## @returns The serialized input event 
 func serialize_keybind(key: InputEvent) -> Dictionary:
 	if key is InputEventJoypadButton:
 		return {"JoypadButton": key.button_index}
@@ -125,8 +147,13 @@ func serialize_keybind(key: InputEvent) -> Dictionary:
 		return {"Key": key.keycode}
 	return {"Unknown": 0}
 
+
 ## Takes in a stringified dictionary, parses it into a string, then creates an 
 ## input event with the information stored. 
+## 
+## @param data The stringified dictionary representing a key to parse
+## @param device The players device id 
+## @returns The parsed input event 
 func parse_keybind(data: String, device: int) -> InputEvent:
 	var input = "UNBOUND"
 	var json = JSON.parse_string(data) 
@@ -142,9 +169,12 @@ func parse_keybind(data: String, device: int) -> InputEvent:
 	
 	if typeof(input) == TYPE_OBJECT : input.device = device 
 	return input
-		
-		
-func load_all_keybind_settings():
+
+
+## Loads the keybind settings from the config file
+##
+## @returns A dictionary of the keybind settings 
+func load_all_keybind_settings() -> Dictionary:
 	var keybind_settings = []
 	for i in 8:
 		keybind_settings.append(load_settings_helper(str("keybinds", i)))
--- godot/scripts/rebind.gd
+++ godot/scripts/rebind.gd
@@ -1,16 +1,27 @@
 extends Control
+## Handles the logic for rebinding a button.
+## 
+## @author Trinity Pittman
+## @version Spring 2025
 
+## The label that contains what action the rebind button controls
 @onready var label: Label = $HBoxContainer/Label as Label
+## The button to rebind a key, shows what key an action is bound to 
 @onready var button: Button = $HBoxContainer/Button as Button
 
+## The action name (jump, sprint, etc.)
 @export var action_name: String = "Unassigned"
+## The last event that was bound to this action
 var latest_event: InputEvent = null
 
-func _ready():
+## Called when the node enters the scene. Sets the label depending on the action
+## name. Also turns of unhandled input processing. 
+func _ready() -> void:
 	set_process_unhandled_input(false) 
 	set_action_label()
 	
 	
+## Sets the action label based on the action name. 
 func set_action_label() -> void:
 	label.text = "Unassigned"
 
@@ -36,15 +47,28 @@ func set_action_label() -> void:
 			label.text = "Steel"
 	
 
+## Sets the button text given the keybind settings, also recieves the latest 
+## event and stores this. 
+## 
+## @param keybind_settings A dictionary containing all of the keybinds for a 
+## single player
+## @param event The last event that was keybound to the action 
 func set_button_text(keybind_settings, event: InputEvent) -> void:
 	button.text = keybind_settings[action_name]
 	latest_event = event
 
 
+## Called when a rebind button is pressed. This function will set the text for 
+## the button to prompt the user to press a new key. It will then turn on 
+## processing of unhandled input and make all other keybind buttons 
+## untoggleable.
+## 
+## @param toggled_on Whether the button was toggled on or off
 func _on_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		button.text = "Press a new key"
-		set_process_unhandled_input(true) # Catches input other nodes didn't handle
+		# Catches input other nodes didn't handle
+		set_process_unhandled_input(true) 
 		
 	for i in get_tree().get_nodes_in_group("keybind_btns"):
 		if i.action_name != self.action_name:
@@ -52,7 +76,9 @@ func _on_button_toggled(toggled_on: bool) -> void:
 			i.set_process_unhandled_input(false)
 
 
-## When we press a key it will untoggle the button
+## When we press a key it will untoggle the button and call rebind
+## 
+## @param event The unhandled input event that was caught
 func _unhandled_input(event: InputEvent) -> void:
 	print("UNHANDLED: %s DEVICE: %s" %[event, event.device])
 	
@@ -64,18 +90,21 @@ func _unhandled_input(event: InputEvent) -> void:
 		rebind_action_key(event)
 		button.button_pressed = false
 
-
-func rebind_action_key(event) -> void:
-	print("REBIND TO: ", event)
+## Rebinds an action to a new key. 
+## 
+## @param event The event to remap the action to.
+func rebind_action_key(event: InputEvent) -> void:
+	# Remove the event currently bound to the action if one exists
 	if latest_event != null: 
-		InputMap.action_erase_event(action_name, latest_event) # Not working?
-
+		InputMap.action_erase_event(action_name, latest_event) 
 	
-	# Add a check that unbinds if u double bind a key
+	# Add the event and stop processing input
 	InputMap.action_add_event(action_name, event)
 	set_process_unhandled_input(false) 
 	button.text = event.as_text()
 	
+	# Check to make sure no other actions have the same event 
 	for rebind in get_tree().get_nodes_in_group("keybind_btns"):
+		# If one does, unbind it 
 		if rebind.button.text == button.text and rebind.button != button:
 			rebind.button.emit_signal("toggled", true)
