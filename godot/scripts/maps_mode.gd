11033db5045189877e5f4ee5d9325d90883eac88
trinpanda <149008043+trinpanda@users.noreply.github.com>
2025-03-27 21:42:45 -0400

    Game Mode selecting functionality in (not tested)

--- godot/scripts/maps_mode.gd
+++ godot/scripts/maps_mode.gd
@@ -1,11 +1,16 @@
 extends Control
-@onready var map_one: TextureButton = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel/VBoxContainer/TextureButton1
-@onready var map_two: TextureButton = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel2/VBoxContainer2/TextureButton2
-@onready var map_three: TextureButton = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel3/VBoxContainer2/TextureButton3
+@onready var map_one: TextureButton = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel/VBoxContainer/TextureButton1
+@onready var map_two: TextureButton = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel2/VBoxContainer2/TextureButton2
+@onready var map_three: TextureButton = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel3/VBoxContainer2/TextureButton3
 
-@onready var panel_1: Panel = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel
-@onready var panel_2: Panel = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel2
-@onready var panel_3: Panel = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Panel3
+@onready var panel_1: Panel = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel
+@onready var panel_2: Panel = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel2
+@onready var panel_3: Panel = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer/Panel3
+
+@onready var game_mode_1: Button = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer2/VBoxContainer/Button
+@onready var game_mode_2: Button = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer2/VBoxContainer3/Button
+@onready var game_mode_3: Button = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/Button
+
 
 const MAP_PRESSED = preload("res://assets/Maps/map_pressed.tres")
 const MAP_UNPRESSED = preload("res://assets/Maps/map_unpressed.tres")
@@ -14,20 +19,26 @@ const MAP_UNPRESSED = preload("res://assets/Maps/map_unpressed.tres")
 
 var panels = []
 var map_button_group = ButtonGroup.new()
+var mode_button_group = ButtonGroup.new()
 
 func _ready() -> void:
 	map_one.button_group = map_button_group
 	map_two.button_group = map_button_group
 	map_three.button_group = map_button_group
 	
-	map_button_group.connect("pressed", on_button_press)
+	game_mode_1.button_group = mode_button_group
+	game_mode_2.button_group = mode_button_group
+	game_mode_3.button_group = mode_button_group
+	
+	map_button_group.connect("pressed", on_map_button_press)
+	mode_button_group.connect("pressed", on_mode_button_press)
 	
 	panels.append(panel_1)
 	panels.append(panel_2)
 	panels.append(panel_3)
 	
 
-func on_button_press(button: BaseButton) -> void:
+func on_map_button_press(button: BaseButton) -> void:
 	var map = ""
 	match button:
 		map_one:
@@ -44,4 +55,16 @@ func on_button_press(button: BaseButton) -> void:
 		else:
 			panels[i].add_theme_stylebox_override("panel", MAP_UNPRESSED)
 	
-	print(map)
+	
+# TODO needs to be filled out with the game modes we will have 
+func on_mode_button_press(button: BaseButton) -> void:
+	var mode = ""
+	match button:
+		game_mode_1:
+			mode = "Last Player Standing"
+		game_mode_2:
+			mode = ""
+		game_mode_3:
+			mode = ""
+	game.set_game_mode(mode)
+		
